b59481aadfc0ae992831fbec5a3c5462dba2176a
OkuhleNdlebe <218338619@mycput.ac.za>
2021-04-01 18:38:56 +0200

    Final commit

--- src/main/java/za/ac/cput/adp3Assignment/Area.java
+++ src/main/java/za/ac/cput/adp3Assignment/Area.java
@@ -1,3 +1,12 @@
+/**
+ * Okuhle Ndlebe
+ * Application Development Practice 3
+ * Assignment 1
+ * Test Driven Development
+ * 218338619
+ * due date 01 April 2021
+ * Group Assignment
+ */
 package za.ac.cput.adp3Assignment;
 
 public class Area {
--- src/test/java/za/ac/cput/adp3Assignment/AreaTest.java
+++ src/test/java/za/ac/cput/adp3Assignment/AreaTest.java
@@ -1,14 +1,69 @@
 package za.ac.cput.adp3Assignment;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+//import static org.junit.Assert.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AreaTest {
-
+private Area area;
     @BeforeEach
-    void setUp() {
+   void setUp() throws Exception {
+        area = new Area();
     }
+    @AfterEach
+    void tearDown() throws Exception{
+
+    }
+    @Test
+    void areaEquality(){
+        int result = area.area (4, 12);
+        //Testing Object Equality
+        assertEquals(48, result);
+    }
+
+    @Test
+     void  areaIdentity(){
+        int result = area.area(2,6);
+        //Testing Object Identity
+        assertSame(12, result);
+
+    }
+
+    @Test
+    void areaFailingTest(){
+        int result = area.area(2,6);
+        //Testing Failing Test
+        fail("This test fail");
+    }
+    //Testing Timeouts
+    @Test
+    @Timeout(100)
+    void areaTimeout(){
+        int result = area.area(2,6);
+        assertEquals(12, result);
+        System.out.println("Test completed on time");
+    }
+
+    //Testing Disabling Tests
+    @Test
+    @Disabled
+    void areaDisablingTest(){
+        int result = area.area(2,6);
+
+        assertEquals(12,result);
+        System.out.println("Test successful disabled at this branch");
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 }
