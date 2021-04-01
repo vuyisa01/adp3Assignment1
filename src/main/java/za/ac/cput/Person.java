51d2506a3507f13027e48bad5db315eda900adc9
Legiste <legistendabashinze@gmail.com>
2021-04-01 14:04:27 +0200

    First Commit: Added Person.java with methods to my repository - 217046207

---
+++ src/main/java/za/ac/cput/Person.java
@@ -0,0 +1,55 @@
+package za.ac.cput;
+
+public class Person {
+
+    public String personName;
+    public String personSurname;
+    public int personAge;
+    public String personGender;
+
+    public Person(){
+
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonSurname() {
+        return personSurname;
+    }
+
+    public void setPersonSurname(String personSurname) {
+        this.personSurname = personSurname;
+    }
+
+    public int getPersonAge() {
+        return personAge;
+    }
+
+    public void setPersonAge(int personAge) {
+        this.personAge = personAge;
+    }
+
+    public String getPersonGender() {
+        return personGender;
+    }
+
+    public void setPersonGender(String personGender) {
+        this.personGender = personGender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personName='" + personName + '\'' +
+                ", personSurname='" + personSurname + '\'' +
+                ", personAge=" + personAge +
+                ", personGender='" + personGender + '\'' +
+                '}';
+    }
+}
