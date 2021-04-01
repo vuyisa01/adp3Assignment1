c7db444a4481e5e6ed216daf604a582224aa8630
Legiste <legistendabashinze@gmail.com>
2021-04-01 14:18:08 +0200

    Second Commit: Added PersonTest.java with empty methods to my repository - 217046207

---
+++ src/test/java/za/ac/cput/PersonTest.java
@@ -0,0 +1,37 @@
+package za.ac.cput;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testObejectEquality() {
+
+    }
+
+    @Test
+    public void testOjectIdentity() {
+
+    }
+
+    @Test
+    public void testFailingTest() {
+
+    }
+
+    @Test
+    public void testDisablingTest() {
+
+    }
+
+
+
+}
